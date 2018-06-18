3df8734f965b003a88510ba7bce8c186e2467ef9
Markus Klemm <markus@markus@markusklemm.net>
2018-06-18 16:53:09 +0200

    More cleanup of shader init code

--- src/opengl_helper.cpp
+++ src/opengl_helper.cpp
@@ -1,7 +1,13 @@
 #include "opengl_helper.hpp"
 
 #include <GL/glew.h>
-#include "LoadShader.hpp"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <tuple>
+#include <vector>
 
 
 namespace {
@@ -18,6 +24,9 @@ GLuint VAOs[NumVAOs];
 GLuint Buffers[NumBuffers];
 const GLuint NumVertices = 6;
 GLuint program;
+
+GLuint loadShaders(std::map<mk::opengl_helper::shader_type, std::tuple<std::string, GLuint>> &shaders);
+
 }
 
 const std::string vertexShader = {"#version 330 core\n"
@@ -44,7 +53,11 @@ mk::opengl_helper::opengl_helper() {
     glewExperimental = GL_TRUE;
     if (glewInit())
         throw std::runtime_error("glewInit failed");
-    program = loadShaders(vertexShader, fragmentShader, "", "", "", "");
+    std::map<shader_type, std::string> shaders{
+            {shader_type::vertex_shader,   vertexShader},
+            {shader_type::fragment_shader, fragmentShader},
+    };
+    program = this->set_shaders(shaders);
     glUseProgram(program);
     glGenBuffers(NumBuffers, Buffers);//NumBuffers = 1
     glGenVertexArrays(NumVAOs, VAOs);//NumVAOs = 1
@@ -64,6 +77,43 @@ mk::opengl_helper::opengl_helper() {
 
 mk::opengl_helper::~opengl_helper() {
     glDeleteBuffers(NumBuffers, Buffers);
+}
+
+uint32_t mk::opengl_helper::shader_type_to_gl_enum(shader_type shader_type) {
+    uint32_t value{};
+    switch (shader_type) {
+        case shader_type::vertex_shader:
+            value = GL_VERTEX_SHADER;
+            break;
+        case shader_type::fragment_shader:
+            value = GL_FRAGMENT_SHADER;
+            break;
+        case shader_type::geometry_shader:
+            value = GL_GEOMETRY_SHADER;
+            break;
+        case shader_type::tessellation_control_shader:
+            value = GL_TESS_CONTROL_SHADER;
+            break;
+        case shader_type::tessellation_evaluation_shader:
+            value = GL_TESS_EVALUATION_SHADER;
+            break;
+        case shader_type::compute_shader:
+            value = GL_COMPUTE_SHADER;
+            break;
+        default:
+            value = 0;
+    }
+
+    return value;
+}
+
+uint32_t mk::opengl_helper::set_shaders(const std::map<mk::opengl_helper::shader_type, std::string> &shaders) {
+    std::map<shader_type, std::tuple<std::string, GLuint>> transformed_shaders;
+    for (const auto &shader: shaders) {
+        transformed_shaders[shader.first] = std::make_tuple(shader.second, 0);
+    }
+
+    return loadShaders(transformed_shaders);
 }
 
 void mk::opengl_helper::praktikum01_1() {
@@ -86,5 +136,64 @@ void mk::opengl_helper::praktikum01_2() {
     };
 }
 
+namespace {
+GLuint loadShaders(std::map<mk::opengl_helper::shader_type, std::tuple<std::string, GLuint>> &shaders) {
+
+    GLint infoLogLength;
+    GLint result = GL_FALSE;
+
+    for (auto &shader : shaders) {
+        const auto &shaderCode = std::get<0>(shader.second);
+        GLuint &shaderId = std::get<1>(shader.second);
+        std::get<1>(shader.second) = glCreateShader(mk::opengl_helper::shader_type_to_gl_enum(shader.first));
+        if (shaderCode.empty())
+            continue;
+        std::cout << "compiling shader: " << shaderCode << "\n";
+        char const *vertexShaderPointer = shaderCode.c_str();
+        const GLint vertexSourceLength = shaderCode.size();
+        glShaderSource(shaderId, 1, &vertexShaderPointer, &vertexSourceLength);
+        glCompileShader(shaderId);
+
+        glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
+        if (result == GL_FALSE) {
+            printf("shader putt\n");
+        }
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
+        if (infoLogLength > 0) {
+            std::vector<char> vertexShaderErrorMsg(infoLogLength);
+            glGetShaderInfoLog(shaderId, infoLogLength, NULL, &vertexShaderErrorMsg[0]);
+            printf("%s\n", &vertexShaderErrorMsg[0]);
+        }
+
+    }
+
+    std::cout << "linking program\n";
+    GLuint programID = glCreateProgram();
+
+    for (const auto &shader: shaders) {
+        const auto &shaderCode = std::get<0>(shader.second);
+        const auto &shaderId = std::get<1>(shader.second);
+        if (!shaderCode.empty()) {
+            glAttachShader(programID, shaderId);
+        }
+    }
+    glLinkProgram(programID);
+
+    glGetProgramiv(programID, GL_LINK_STATUS, &result);
+    glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+    if (infoLogLength > 0) {
+        std::vector<GLchar> progErrorMsg(infoLogLength);
+        glGetProgramInfoLog(programID, infoLogLength, NULL, progErrorMsg.data());
+        std::cerr << progErrorMsg.data() << "\n";
+    }
+
+    for (const auto &shader: shaders) {
+        const auto &shaderCode = std::get<0>(shader.second);
+        const auto &shaderId = std::get<1>(shader.second);
+        glDeleteShader(shaderId);
+    }
 
 
+    return programID;
+}
+}
--- src/opengl_helper.hpp
+++ src/opengl_helper.hpp
@@ -1,6 +1,10 @@
 #ifndef CG1_S70357_2017_OPENGL_HELPER_HPP
 #define CG1_S70357_2017_OPENGL_HELPER_HPP
 
+#include <string>
+#include <map>
+#include <cstdint>
+
 namespace mk {
 
 /*! @brief Utility class for glew and opengl
@@ -15,9 +19,23 @@ public:
 
     ~opengl_helper();
 
+    enum struct shader_type {
+        vertex_shader,
+        fragment_shader,
+        geometry_shader,
+        tessellation_control_shader,
+        tessellation_evaluation_shader,
+        compute_shader,
+    };
+
+    static uint32_t shader_type_to_gl_enum(shader_type);
+
     void praktikum01_1();
 
     void praktikum01_2();
+
+private:
+    uint32_t set_shaders(const std::map<shader_type, std::string> &shaders);
 
 };
 
