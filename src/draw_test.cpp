1f41a52fb7bbd4805c9dc909b66aac6118670ae9
Markus Klemm <markus@markus@markusklemm.net>
2021-10-04 23:41:37 +0200

    renaming

--- src/draw_test.cpp
+++ src/draw_test.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-
 #include <mk_sdl2/sdl2_vulcan_helper.hpp>
 #include <mk_sdl2/sdl2_helpers.hpp>
-#include <mk_sdl2/sdl2_audio_helper.hpp>
 
 #include <cstdlib>
 
@@ -11,7 +8,7 @@ int main() {
     try {
         mk::sdl2_vulcan_helper sdl_vulcan_helper{};
         mk::sdl2_event_helper event_helper{};
-        mk::sdl2_audio_helper audio_helper{};
+
         mk::sdl2_cout cout{};
 
         event_helper.replace_event_handler(std::bind(&mk::sdl2_vulcan_helper::redraw, std::ref(sdl_vulcan_helper)));
